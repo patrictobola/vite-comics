6740279ebdfbd3dc97bb1dc08bb47e0ef47c6229
patrictobola <tobola92@gmail.com>
2023-06-29 15:05:26 +0200

    first main section

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -3,5 +3,22 @@ export default {
 
 }
 </script>
-<template></template>
-<style></style>
+<template>
+    <div>
+        <div class="container">
+            <h1>Content goes here</h1>
+        </div>
+    </div>
+</template>
+<style scoped>
+h1 {
+    color: white;
+}
+
+div {
+    height: 120px;
+    display: flex;
+    align-items: center;
+    background-color: slategray;
+}
+</style>
