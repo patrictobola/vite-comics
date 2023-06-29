b73e7f4dfd42601949b5995847c5ef0be54af3d9
patrictobola <tobola92@gmail.com>
2023-06-29 15:00:14 +0200

    header done

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,31 @@
-<script></script>
-<template></template>
-<style></style>
+<script>
+import AppFooter from './components/AppFooter.vue';
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+// Supports weights 300-800
+import '@fontsource-variable/open-sans';
+export default {
+  components: {
+    AppFooter,
+    AppHeader,
+    AppMain,
+  }
+}
+</script>
+<template>
+  <AppHeader />
+  <AppMain />
+  <AppFooter />
+</template>
+
+<style>
+.container {
+  width: 1200px;
+  margin: 0 auto;
+}
+
+body {
+  font-family: 'Open Sans Variable', sans-serif;
+  background-color: #000;
+}
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,131 @@
+<script>
+export default {
+    data() {
+        return {
+            NavbarLinks: [
+                {
+                    text: 'Characters',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Comics',
+                    url: '#',
+                    current: true,
+                },
+                {
+                    text: 'Movies',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'TV',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Games',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Collectibles',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Videos',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Fans',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'News',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Shop',
+                    url: '#',
+                    current: false,
+                },
+            ]
+
+        }
+    }
+}
+</script>
+<template>
+    <nav>
+        <div id="navbar" class="container">
+
+            <div class="logo">
+                <img src="../assets/img/dc-logo.png" alt="">
+            </div>
+            <div class="links">
+                <ul>
+                    <li v-for="link in this.NavbarLinks" :key="link.text"><a :href="link.url">{{ link.text }}</a></li>
+                </ul>
+
+            </div>
+        </div>
+    </nav>
+</template>
+
+<style lang="scss" scoped>
+nav {
+    height: 100px;
+    background-color: white;
+
+}
+
+#navbar {
+    justify-content: space-between;
+}
+
+div {
+    height: 100%;
+    display: flex;
+    align-items: center;
+}
+
+img {
+    height: 80px;
+}
+
+ul {
+    height: 100%;
+    margin: 0;
+}
+
+li {
+    list-style-type: none;
+    padding: 0 10px;
+    font-weight: 500;
+    text-transform: uppercase;
+    height: 100%;
+
+
+}
+
+a {
+    text-decoration: none;
+    color: black;
+    height: 98%;
+    display: flex;
+    align-items: center;
+
+    &:hover {
+        color: lightblue;
+        border-bottom: 2px solid lightblue;
+    }
+}
+
+ul {
+    display: flex;
+}
+</style>
